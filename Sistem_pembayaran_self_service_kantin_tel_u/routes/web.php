0bf6192b317b8c6f8c59464e6d27b50ad7805734
Zakiyymj <zakiyymj@gmail.com>
2025-05-27 21:02:46 +0700

    commit menu controller

--- Sistem_pembayaran_self_service_kantin_tel_u/routes/web.php
+++ Sistem_pembayaran_self_service_kantin_tel_u/routes/web.php
@@ -1,7 +1,9 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\MenuController;
 
+Route::resource('menus', MenuController::class);
 Route::get('/', function () {
     return view('welcome');
 });
