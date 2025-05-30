f866ec4fce16ddefa7e13d80cb62e1447a2fd066
fnann117 <afnan.huwaiza@gmail.com>
2025-05-30 19:32:17 +0700

    migration

--- Sistem_pembayaran_self_service_kantin_tel_u/database/migrations/2025_05_30_075608_create_transactions_table.php
+++ Sistem_pembayaran_self_service_kantin_tel_u/database/migrations/2025_05_30_075608_create_transactions_table.php
@@ -9,12 +9,12 @@ return new class extends Migration
     /**
      * Run the migrations.
      */
-    public function up()
+    public function up(): void
     {
         Schema::create('transactions', function (Blueprint $table) {
             $table->id('transaction_id');
-            $table->foreignId('order_id')->constrained('orders', 'order_id');
-            $table->foreignId('user_id')->constrained('users', 'user_id');
+            $table->foreignId('order_id')->constrained('orders', 'order_id')->onDelete('cascade');
+            $table->foreignId('user_id')->constrained('users', 'user_id')->onDelete('cascade');
             $table->enum('payment_method', ['cash', 'e-wallet', 'bank_transfer']);
             $table->enum('status', ['pending', 'success', 'failed']);
             $table->decimal('amount', 10, 2);
