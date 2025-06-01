3d62bd931021f9d648061b2bb559630d18153325
fnann117 <afnan.huwaiza@gmail.com>
2025-06-01 19:26:39 +0700

    route

--- Sistem_pembayaran_self_service_kantin_tel_u/routes/web.php
+++ Sistem_pembayaran_self_service_kantin_tel_u/routes/web.php
@@ -1,7 +1,23 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Admin\FavoriteMenuController;
+use App\Http\Controllers\DashboardController;
+
 
 Route::get('/', function () {
-    return view('welcome');
+    return redirect()->route('dashboard');
+});
+
+Route::get('/dashboard', [DashboardController::class, 'showdashboard'])->name('dashboard');
+Route::get('/pesan-makanan', [DashboardController::class, 'pesanMakanan'])->name('pesan.makanan');
+Route::get('/riwayat-transaksi', [DashboardController::class, 'riwayatTransaksi'])->name('riwayat.transaksi');
+Route::get('/kelola-transaksi', [DashboardController::class, 'kelolaTransaksi'])->name('kelola.transaksi');
+
+Route::prefix('admin')->name('admin.')->group(function () {
+    Route::resource('favorite-menus', FavoriteMenuController::class);
+});
+
+Route::fallback(function () {
+    return response()->view('errors.404', [], 404);
 });
