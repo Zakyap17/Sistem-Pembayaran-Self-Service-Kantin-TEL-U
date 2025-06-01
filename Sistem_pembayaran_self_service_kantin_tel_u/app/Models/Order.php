1604a7be2baea0e004a421203adf26ac2acd9856
Nando <zlvyynnnn@gmail.com>
2025-06-01 14:09:57 +0700

    Menambahkan fillable dan relasi model Order dan OrderDetail

--- Sistem_pembayaran_self_service_kantin_tel_u/app/Models/Order.php
+++ Sistem_pembayaran_self_service_kantin_tel_u/app/Models/Order.php
@@ -3,8 +3,23 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use App\Models\User;
+use App\Models\OrderDetail;
 
 class Order extends Model
 {
-    //
+    use HasFactory;
+
+    protected $fillable = [
+        'user_id',
+        'status',
+        'total_amount',
+    ];
+    public function user() {
+        return $this->belongsTo(User::class);
+    }
+    public function details() {
+        return $this->hasMany(OrderDetail::class);
+    }
 }
--- Sistem_pembayaran_self_service_kantin_tel_u/app/Models/OrderDetail.php
+++ Sistem_pembayaran_self_service_kantin_tel_u/app/Models/OrderDetail.php
@@ -3,8 +3,26 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use App\Models\Menu;
+use App\Models\Order;
 
 class OrderDetail extends Model
 {
-    //
+    use HasFactory;
+
+    protected $fillable = [
+        'order_id',
+        'menu_id',
+        'quantity',
+        'subtotal',
+    ];
+
+    public function order() {
+        return $this->belongsTo(Order::class);
+    }
+
+    public function menu() {
+        return $this->belongsTo(Menu::class);
+    }
 }
